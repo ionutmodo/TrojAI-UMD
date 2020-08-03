05679186c423e1cb1b3adc199a7a14824e85e6af
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-08-03 23:17:22 +0300

    implemented forward with activations for IC inside DenseBlock

--- architectures/SDNs/SDNDenseNet121.py
+++ architectures/SDNs/SDNDenseNet121.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,10 +9,102 @@ import tools.aux_funcs as af
 from architectures.SDNs.SDNConfig import SDNConfig
 
 
+def _forward_w_acts_for_end_DenseBlock(x, net_features):
+    activations = []
+    out = None
+    # the for below works with internal layers of "features"
+    for layer in net_features:  # children[0] is the Sequential containing convolutional blocks (called 'features')
+        out = layer(x if out is None else out)
+        if isinstance(layer, (pooling.MaxPool2d, densenet._DenseBlock, densenet._Transition)):
+            """get the logits at this output stage"""
+            activation_3x = af.reduce_activation(out)
+            activations.append(activation_3x)
+    return activations, out
+
+
+def _forward_w_acts_for_end_DenseBlock(x, net_features):
+    activations = []
+    out = x
+    # the for below works with internal layers of "features"
+    for layer in net_features:  # children[0] is the Sequential containing convolutional blocks (called 'features')
+        out = layer(out)
+        if isinstance(layer, (pooling.MaxPool2d, densenet._DenseBlock, densenet._Transition)):
+            """get the logits at this output stage"""
+            activations.append(af.reduce_activation(out))
+    return activations, out
+
+
+def _forward_w_acts_for_inside_DenseBlock(x, net_features):
+    activations = []
+    out = x
+    # Returns the logits of each DenseLayer inside any DenseBlock. Will attach to many ICs
+    for layer in net_features:
+        if isinstance(layer, (conv.Conv2d, batchnorm.BatchNorm2d, activation.ReLU)):
+            # just forward x because we don't attach any IC here (we are at the early hidden layers of network)
+            out = layer(out)
+        elif isinstance(layer, (pooling.MaxPool2d, densenet._Transition)):
+            # attach an IC at maxpool and Transition
+            out = layer(out)
+            activations.append(af.reduce_activation(out))
+        elif isinstance(layer, densenet._DenseBlock):
+            # a _DenseBlock will contain many _DenseLayers and we want to attach one IC at each _DenseLayer
+            # the code below is taken from _DenseBlock-forward method
+            features_list = [out]
+            for name, sublayer in layer.items():
+                new_features = sublayer(features_list)
+                features_list.append(new_features)
+                activations.append(af.reduce_activation(new_features))
+            out = torch.cat(features_list, 1)
+    return activations, out
+
+
+def _get_layerwise_params_end_DenseBlock(x, net_features, num_classes):
+    params = [] # will contain tuples (self.num_classes, current_input_size, input_features_for_current_IC)
+    # Returns the logits only at the output of each DenseBlock. Leads to a few number of ICs
+    for layer in net_features:
+        # print(type(layer))
+        x = layer(x)
+        current_size = x.size()[-1]  # the feature maps are always squared
+        n_channels = x.size()[1]  # the number of feature maps
+        if isinstance(layer, (pooling.MaxPool2d, densenet._DenseBlock, densenet._Transition)):
+            # attach an IC at maxpool (before each DenseBlock), DenseBlocks and Transitions
+            params.append((num_classes, current_size, n_channels))
+    return params
+
+
+def _get_layerwise_params_inside_DenseBlock(x, net_features, num_classes):
+    params = [] # will contain tuples (self.num_classes, current_input_size, input_features_for_current_IC)
+    # Returns the logits of each DenseLayer inside any DenseBlock. Will attach to many ICs
+    for layer in net_features:
+        # print(type(layer))
+        current_size = x.size()[-1]  # the feature maps are always squared
+        n_channels = x.size()[1]  # the number of feature maps
+        if isinstance(layer, (conv.Conv2d, batchnorm.BatchNorm2d, activation.ReLU)):
+            # just forward x because we don't attach any IC here (we are at the early hidden layers of network)
+            x = layer(x)
+        elif isinstance(layer, (pooling.MaxPool2d, densenet._Transition)):
+            # attach an IC at maxpool and Transition
+            x = layer(x)
+            params.append((num_classes, current_size, n_channels))
+        elif isinstance(layer, densenet._DenseBlock):
+            # a _DenseBlock will contain many _DenseLayers and we want to attach one IC at each _DenseLayer
+            # the code below is taken from _DenseBlock-forward method
+            features_list = [x]
+            for name, sublayer in layer.items():
+                # print(name, sublayer)
+                new_features = sublayer(features_list)
+                features_list.append(new_features)
+                sublayer_current_size = new_features.size()[-1]  # the feature maps are always squared
+                sublayer_n_channels = new_features.size()[1]  # the number of feature maps
+                params.append((num_classes, sublayer_current_size, sublayer_n_channels))
+            x = torch.cat(features_list, 1)
+    return params
+
+
 class SDNDenseNet121(nn.Module):
     def __init__(self, model, input_size, num_classes, sdn_type, device):
         super(SDNDenseNet121, self).__init__()
-        assert sdn_type == SDNConfig.DenseNet_end_DenseBlock, 'Only DenseNet_end_DenseBlock mode is supported now'
+        assert sdn_type in SDNConfig.DenseNet, 'SDNDenseNet121:init - Parameter sdn_type must be in SDNConfig.DenseNet'
         self.model = model
         self.input_size = input_size
         self.num_classes = num_classes
@@ -22,25 +116,20 @@ class SDNDenseNet121(nn.Module):
         return fwd
 
     def forward_w_acts(self, x):
-        activations = []
         net_features, net_classifier = list(self.model.children())
-        out = None
-        # the for below works with internal layers of "features"
-        for layer in net_features:  # children[0] is the Sequential containing convolutional blocks (called 'features')
-            out = layer(x if out is None else out)
-            if isinstance(layer, (pooling.MaxPool2d, densenet._DenseBlock, densenet._Transition)):
-                """get the logits at this output stage"""
-                activation_3x = af.reduce_activation(out)
-                activations.append(activation_3x)
-                # print(tuple(out.size()))
-                # print(tuple(activation_3x.size()))
-                # print()
+
+        if self.sdn_type == SDNConfig.DenseNet_end_DenseBlock:
+            activations, out = _forward_w_acts_for_end_DenseBlock(x, net_features)
+        elif self.sdn_type == SDNConfig.DenseNet_inside_DenseBlock:
+            activations, out = _forward_w_acts_for_inside_DenseBlock(x, net_features)
+        else:
+            raise RuntimeError('SDNDenseNet121:forward_w_acts - invalid value for parameter sdn_type')
+
         # the code below is taken from source code of DenseNet.forward
         out = F.relu(out, inplace=True)
         out = F.adaptive_avg_pool2d(out, (1, 1))
         out = torch.flatten(out, 1)
         out = net_classifier(out)  # forward pass the very last feature (output of children[0]) through FC layer at the end
-        # print(out.size())
         return activations, out
 
     def get_layerwise_model_params(self):
@@ -67,44 +156,11 @@ class SDNDenseNet121(nn.Module):
                 Linear layer that we are not interested in attaching any IC here, so we don't use it
         """
         x = torch.zeros(self.input_size).to(self.device)  # an input image to forward through the network to get the input size of each IC
-        params = []  # will contain tuples (self.num_classes, current_input_size, input_features_for_current_IC)
-        children = list(self.model.children())
-        net_features, net_classifier = children[0], children[1]  # iterate only through Sequential variable called "features" at index 0
+        net_features = list(self.model.children())[0] # iterate only through Sequential variable called "features" at index 0
 
         if self.sdn_type == SDNConfig.DenseNet_end_DenseBlock:  # attach_IC_at_DenseBlock_end
-            # Returns the logits only at the output of each DenseBlock. Leads to a few number of ICs
-            for layer in net_features:
-                # print(type(layer))
-                x = layer(x)
-                current_size = x.size()[-1]  # the feature maps are always squared
-                n_channels = x.size()[1]  # the number of feature maps
-                if isinstance(layer, (pooling.MaxPool2d, densenet._DenseBlock, densenet._Transition)):
-                    # attach an IC at maxpool (before each DenseBlock), DenseBlocks and Transitions
-                    params.append((self.num_classes, current_size, n_channels))
-            return params
+            return _get_layerwise_params_end_DenseBlock(x, net_features, self.num_classes)
         elif self.sdn_type == SDNConfig.DenseNet_inside_DenseBlock:  # attach_IC_at_each_DenseLayer_in_DenseBlock
-            # Returns the logits of each DenseLayer inside any DenseBlock. Will attach to many ICs
-            for layer in net_features:
-                # print(type(layer))
-                current_size = x.size()[-1]  # the feature maps are always squared
-                n_channels = x.size()[1]  # the number of feature maps
-                if isinstance(layer, (conv.Conv2d, batchnorm.BatchNorm2d, activation.ReLU)):
-                    # just forward x because we don't attach any IC here (we are at the early hidden layers of network)
-                    x = layer(x)
-                elif isinstance(layer, (pooling.MaxPool2d, densenet._Transition)):
-                    # attach an IC at maxpool and Transition
-                    x = layer(x)
-                    params.append((self.num_classes, current_size, n_channels))
-                elif isinstance(layer, densenet._DenseBlock):
-                    # a _DenseBlock will contain many _DenseLayers and we want to attach one IC at each _DenseLayer
-                    # the code below is taken from _DenseBlock-forward method
-                    features_list = [x]
-                    for name, sublayer in layer.items():
-                        # print(name, sublayer)
-                        new_features = sublayer(features_list)
-                        features_list.append(new_features)
-                        sublayer_current_size = new_features.size()[-1]  # the feature maps are always squared
-                        sublayer_n_channels = new_features.size()[1]  # the number of feature maps
-                        params.append((self.num_classes, sublayer_current_size, sublayer_n_channels))
-                    x = torch.cat(features_list, 1)
-            return params
+            return _get_layerwise_params_inside_DenseBlock(x, net_features, self.num_classes)
+        else:
+            raise RuntimeError('get_layerwise_model_params: Invalid sdn_type')
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os, sys
 import torch
 from architectures.SDNs.SDNConfig import SDNConfig
 from architectures.SDNs.SDNDenseNet121 import SDNDenseNet121
+from data import TrojAI
 from settings import *
 # def build_SDN_from_TrojAI_model(model, input_size, num_classes=5):
 #     # if isinstance(model, inception.Inception3):
@@ -10,29 +11,35 @@ from settings import *
 #         return TrojAI_DenseNet121_to_SDN(model, input_size, num_classes)
 #     raise RuntimeError('Unknown TrojAI architecture')
 
-
-if __name__ == '__main__':
+def test_SDN_for_TrojAI():
     os.chdir('/mnt/storage/Cloud/MEGA/TrojAI/data/trojai-round0-dataset')
     id_dense = 5
     id_resnet = 3
     id_inception = 2
+    device = 'cuda'
 
     for i in [id_dense, id_resnet, id_inception]:
         model_name = f'id-{i:08d}/model.pt'
-        print(model_name)
-        sys.exit(666)
         model = torch.load(model_name)
-        model = model.cpu().eval()
+        model = model.to(device).eval()
 
-        trojai_model = SDNDenseNet121(model, TrojAI_input_size, TrojAI_num_classes, SDNConfig.DenseNet_end_DenseBlock)
-        acts, out = trojai_model.forward_w_acts(torch.zeros(TrojAI_input_size))
+        trojai_model = SDNDenseNet121(model,
+                                      TrojAI_input_size,
+                                      TrojAI_num_classes,
+                                      SDNConfig.DenseNet_inside_DenseBlock,
+                                      device)
+        acts, out = trojai_model.forward_w_acts(torch.zeros(TrojAI_input_size).to(device))
+        print(len(acts)+1)
         print(trojai_model.get_layerwise_model_params())
-
-        # params = build_SDN_from_TrojAI_model(model, (1, 3, 224, 224))
-        # print('Total ICs:', len(params))
-        # print(params)
         break
-        # print(summary(model.cuda(), (3, 224, 224)))
-        # for m in model.modules():
-        # for name, param in model.named_parameters()
-        # print('--------------------------------------------------------')
+
+def test_TrojAI_dataset():
+    dataset = TrojAI(folder='/mnt/storage/Cloud/MEGA/TrojAI/data/trojai-round0-dataset/id-00000000/example_data', batch_size=10)
+    for batch in dataset.train_loader:
+        print(batch[0].size(), batch[1].size())
+        break
+
+
+if __name__ == '__main__':
+    test_SDN_for_TrojAI()
+    # test_TrojAI_dataset()
