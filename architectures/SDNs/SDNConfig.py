3514b76821439e0f35499de17c4644dd2b659b34
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-08-05 17:05:10 +0300

    changed config names

--- architectures/SDNs/SDNConfig.py
+++ architectures/SDNs/SDNConfig.py
@@ -1,5 +1,5 @@
 class SDNConfig:
-    DenseNet_end_DenseBlock = 0    # attaches an IC at each DenseBlock end
-    DenseNet_inside_DenseBlock = 1 # attaches an IC at every DenseLayer in a DenseBlock
+    DenseNet_attach_to_DenseBlocks = 0    # attaches an IC at each DenseBlock end
+    DenseNet_attach_to_DenseLayers = 1 # attaches an IC at every DenseLayer in a DenseBlock
 
-    DenseNet = [DenseNet_end_DenseBlock, DenseNet_inside_DenseBlock]
+    DenseNet = [DenseNet_attach_to_DenseBlocks, DenseNet_attach_to_DenseLayers]
