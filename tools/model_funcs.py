3b4bcb7219ed22cbcd965bbb52eb09a7f565eeed
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-08-05 17:16:33 +0300

    testing model at each epoch

--- tools/model_funcs.py
+++ tools/model_funcs.py
@@ -140,7 +140,7 @@ def train_layerwise_classifiers(layerwise_classifiers, data, epochs, optimizer, 
             layerwise_classifiers.train()
             layerwise_classifiers_training_step(layerwise_classifiers, optimizer, batch, device)
 
-        if epoch == 1 or epoch % 3 == 0 or epoch == epochs:
+        if True: # epoch == 1 or epoch % 3 == 0 or epoch == epochs:
             layerwise_ics_test_accs = layerwise_classifiers_test(layerwise_classifiers, data.test_loader, device)
             print('Layerwise ICs Test Accs: {}'.format(layerwise_ics_test_accs))
 
