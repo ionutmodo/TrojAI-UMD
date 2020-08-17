a45e1a73353eade88d83df707f1cf834955f34c1
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-08-17 20:08:09 +0300

    added code to experiment with 5 clean & 5 backdoored models

--- noise_experiments.py
+++ noise_experiments.py
@@ -2,10 +2,12 @@ import os
 import sys
 import torch
 import numpy as np
+import pandas as pd
 from datetime import datetime
 
 import tools.aux_funcs as af
 import tools.model_funcs as mf
+from logistics import get_project_root_path
 from tools.data import ManualData
 from tools.network_architectures import load_trojai_model, get_label_and_confidence_from_logits
 from tools.settings import *
@@ -16,48 +18,75 @@ def show_label_confidence_for_sdn_outputs(outputs):
         label, confidence = get_label_and_confidence_from_logits(out)
         print(f'label = {label}, confidence = {confidence:.4f}')
 
+def get_random_noise(r_type, r_p1, r_p2):
+    noise = None
+    if r_type == 'uniform':
+        noise = np.random.uniform(low=0.0, high=1.0, size=TrojAI_input_size)
+    elif r_type == 'normal':
+        noise = np.random.normal(loc=r_p1, scale=r_p2, size=TrojAI_input_size)
+    else:
+        print('invalid random type! it should be normal or uniform')
+    return np.clip(noise, a_min=0.0, a_max=1.0)
 
-def create_random_noise_dataset(n_samples, cnn_clean, cnn_backdoored, batch_size, device, rand_type, rand_p1, rand_p2):
-    def get_random_noise(r_type, r_p1, r_p2):
-        noise = None
-        if r_type == 'uniform':
-            noise = np.random.uniform(low=0.0, high=1.0, size=TrojAI_input_size)
-        elif r_type == 'normal':
-            noise = np.random.normal(loc=r_p1, scale=r_p2, size=TrojAI_input_size)
-        else:
-            print('invalid random type! it should be normal or uniform')
-        return np.clip(noise, a_min=0.0, a_max=1.0)
-    dataset = []
-    labels_clean = []
-    labels_backdoored = []
-
+def create_random_normal_noise_images(n_samples, param_mean, param_std):
+    noise_images = []
     for _ in range(n_samples):
-        noise_np = get_random_noise(r_type=rand_type, r_p1=rand_p1, r_p2=rand_p2)
+        noise = get_random_noise('normal', param_mean, param_std)
+        noise_images.append(noise)
+    noise_images = np.array(noise_images).squeeze()
+    return noise_images
+
+def label_random_normal_noise_images(data, cnn_model, batch_size):
+    device = cnn_model.device
+    labels = []
+
+    for i in range(data.shape[0]):
+        noise_np = data[i][np.newaxis, :]
         noise_tt = torch.tensor(noise_np, dtype=torch.float, device=device)
 
-        out_clean = cnn_clean(noise_tt)
-        out_backdoored = cnn_backdoored(noise_tt)
+        logits = cnn_model(noise_tt)
+        label, _ = get_label_and_confidence_from_logits(logits)
+        labels.append(label)
 
-        y_clean, _ = get_label_and_confidence_from_logits(out_clean)
-        y_backdoored, _ = get_label_and_confidence_from_logits(out_backdoored)
+    loader = torch.utils.data.DataLoader(ManualData(data, np.array(labels), device),
+                                         batch_size=batch_size,
+                                         shuffle=True,
+                                         num_workers=4 if device == 'cpu' else 0)
+    return loader
 
-        dataset.append(noise_np)
-        labels_clean.append(y_clean)
-        labels_backdoored.append(y_backdoored)
 
-    dataset = np.array(dataset).squeeze()
-    labels_clean = np.array(labels_clean)
-    labels_backdoored = np.array(labels_backdoored)
-
-    dataset_clean = ManualData(dataset, labels_clean, device)
-    dataset_backdoored = ManualData(dataset, labels_backdoored, device)
-
-    num_workers = 4 if device == 'cpu' else 0
-    loader_clean = torch.utils.data.DataLoader(dataset_clean, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-    loader_backdoored = torch.utils.data.DataLoader(dataset_backdoored, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-
-    return loader_clean, loader_backdoored
-
+# def create_random_noise_dataset_2(n_samples, cnn_clean, cnn_backdoored, batch_size, device, rand_type, rand_p1, rand_p2):
+#
+#     dataset = []
+#     labels_clean = []
+#     labels_backdoored = []
+#
+#     for _ in range(n_samples):
+#         noise_np = get_random_noise(r_type=rand_type, r_p1=rand_p1, r_p2=rand_p2)
+#         noise_tt = torch.tensor(noise_np, dtype=torch.float, device=device)
+#
+#         out_clean = cnn_clean(noise_tt)
+#         out_backdoored = cnn_backdoored(noise_tt)
+#
+#         y_clean, _ = get_label_and_confidence_from_logits(out_clean)
+#         y_backdoored, _ = get_label_and_confidence_from_logits(out_backdoored)
+#
+#         dataset.append(noise_np)
+#         labels_clean.append(y_clean)
+#         labels_backdoored.append(y_backdoored)
+#
+#     dataset = np.array(dataset).squeeze()
+#     labels_clean = np.array(labels_clean)
+#     labels_backdoored = np.array(labels_backdoored)
+#
+#     dataset_clean = ManualData(dataset, labels_clean, device)
+#     dataset_backdoored = ManualData(dataset, labels_backdoored, device)
+#
+#     num_workers = 4 if device == 'cpu' else 0
+#     loader_clean = torch.utils.data.DataLoader(dataset_clean, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+#     loader_backdoored = torch.utils.data.DataLoader(dataset_backdoored, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+#
+#     return loader_clean, loader_backdoored
 
 def main():
     np.random.seed(666)
@@ -68,61 +97,77 @@ def main():
     # device = af.get_pytorch_device()
     device = 'cpu'
 
-    id_clean, id_backdoored = 4, 9
     n_samples = 100
     batch_size = int(n_samples / 2)
-    rand_type = 'normal'
+    noise_mean = 0.5
+    noise_std = 0.1
 
-    print(f'n_samples = {n_samples}')
+    clean_model_ids = [4, 7, 25, 27, 40]
+    backdoored_model_ids = [2, 9, 13, 24, 26]
 
-    sdn_path_clean = os.path.join(root_path, f'id-{id_clean:08d}')
-    sdn_path_backdoored = os.path.join(root_path, f'id-{id_backdoored:08d}')
+    stats = pd.DataFrame(columns=['model_id', 'conf_mean', 'conf_std', 'is_backdoored'])
+    n_stats = 0
 
-    sdn_clean      = load_trojai_model(sdn_path_clean,      sdn_name, cnn_name, TrojAI_num_classes, sdn_type, device)
-    sdn_backdoored = load_trojai_model(sdn_path_backdoored, sdn_name, cnn_name, TrojAI_num_classes, sdn_type, device)
+    plots_dir = f'confusion_experiments/noise_experiments/samples-{n_samples}'
+    af.create_path(plots_dir)
 
-    for rand_p1 in [0.25, 0.5, 0.75]:
-        for rand_p2 in [0.01, 0.1, 0.25, 0.5, 0.75, 1.0]:
-            print(f'p1={rand_p1}, p2={rand_p2}')
-            time_start = datetime.now()
-            loader_clean, loader_backdoored = create_random_noise_dataset(n_samples, sdn_clean.model, sdn_backdoored.model, batch_size, device, rand_type,
-                                                                          rand_p1, rand_p2)
+    id_confusion_dict = {} # key=model_id, value=confusion_scores
+    id_loader_dict = {} # key=model_id, value=loader
 
-            # clean_mean, clean_std = mf.sdn_confusion_stats(sdn_clean, loader=loader_clean, device=device)
-            # print(f'clean confusion: mean={clean_mean}, std={clean_std}')
+    print(f'computing confusion for clean models:', end=' ')
+    for id_clean in clean_model_ids:
+        print(id_clean, end=' ')
+        noise_images = create_random_normal_noise_images(n_samples, noise_mean, noise_std)
 
-            clean_confusion_scores = mf.compute_confusion(sdn_clean, loader_clean, device)
-            clean_mean = clean_confusion_scores.mean()
-            clean_std = clean_confusion_scores.std()
-            print(f'clean confusion:      mean={clean_mean:.5f}, std={clean_std:.5f}')
-            # clean_confusion_scores = (clean_confusion_scores - clean_mean) / clean_std
+        sdn_path_clean = os.path.join(root_path, f'id-{id_clean:08d}')
+        sdn_clean = load_trojai_model(sdn_path_clean, sdn_name, cnn_name, TrojAI_num_classes, sdn_type, device)
+        loader_clean = label_random_normal_noise_images(noise_images, sdn_clean.model, batch_size)
 
-            backdoored_confusion_scores = mf.compute_confusion(sdn_backdoored, loader_backdoored, device)
+        clean_confusion_scores = mf.compute_confusion(sdn_clean, loader_clean, device)
+        clean_mean = clean_confusion_scores.mean()
+        clean_std = clean_confusion_scores.std()
+
+        id_confusion_dict[id_clean] = clean_confusion_scores
+        id_loader_dict[id_clean] = loader_clean
+
+        stats.loc[n_stats] = [id_clean, clean_mean, clean_std, 0]
+        n_stats += 1
+    print()
+    print(f'computing confusion for backdoored models:', end=' ')
+    for id_backdoored in backdoored_model_ids:
+        print(id_backdoored, end=' ')
+        noise_images = create_random_normal_noise_images(n_samples, noise_mean, noise_std)
+
+        sdn_path_backdoored = os.path.join(root_path, f'id-{id_backdoored:08d}')
+        sdn_backdoored = load_trojai_model(sdn_path_backdoored, sdn_name, cnn_name, TrojAI_num_classes, sdn_type, device)
+        loader_backdoored = label_random_normal_noise_images(noise_images, sdn_backdoored.model, batch_size)
+
+        backdoored_confusion_scores = mf.compute_confusion(sdn_backdoored, loader_backdoored, device)
+        backdoored_mean = backdoored_confusion_scores.mean()
+        backdoored_std = backdoored_confusion_scores.std()
+
+        id_confusion_dict[id_backdoored] = backdoored_confusion_scores
+        id_loader_dict[id_backdoored] = loader_backdoored
+
+        stats.loc[n_stats] = [id_backdoored, backdoored_mean, backdoored_std, 1]
+        n_stats += 1
+    print()
+    for id_clean in clean_model_ids:
+        clean_confusion_scores = id_confusion_dict[id_clean]
+        clean_mean = clean_confusion_scores.mean()
+        clean_std = clean_confusion_scores.std()
+        for id_backdoored in backdoored_model_ids:
+            backdoored_confusion_scores = id_confusion_dict[id_backdoored]
             backdoored_mean = backdoored_confusion_scores.mean()
             backdoored_std = backdoored_confusion_scores.std()
-            print(f'backdoored confusion: mean={backdoored_mean:.5f}, std={backdoored_std:.5f}')
-            # backdoored_confusion_scores = (backdoored_confusion_scores - clean_mean) / clean_std  # divide backdoored by clean mean/std!
-
-            time_end = datetime.now()
-            print(f'took: {time_end - time_start}')
-
-
-            plots_dir = f'confusion_experiments/noise_experiments'
-            af.create_path(plots_dir)
 
             conf_mean_diff = abs(clean_confusion_scores.mean() - backdoored_confusion_scores.mean())
 
-            if rand_type == 'uniform':
-                save_name = f'noised_uniform_datasets_samples-{n_samples}_ids-{id_clean}-{id_backdoored}.png'
-                title = f'U[0,1) pixels\nclean conf dist: m={clean_mean:.2f}, s={clean_std:.2f}\n' \
-                        f'backd conf dist: m={backdoored_mean:.2f}, s={backdoored_std:.2f}\n' \
-                        f'conf dist means difference = {conf_mean_diff:.2f}'
-            else:
-                save_name = f'noised_normal-{rand_p1:.2f}-{rand_p2:.2f}_datasets_samples-{n_samples}_ids-{id_clean}-{id_backdoored}.png'
-                title = f'N(mean={rand_p1:.2f}, std={rand_p2:.2f}) pixels\n' \
-                        f'clean conf dist: m={clean_mean:.2f}, s={clean_std:.2f}\n' \
-                        f'backd conf dist: m={backdoored_mean:.2f}, s={backdoored_std:.2f}\n' \
-                        f'conf dist means difference = {conf_mean_diff:.2f}'
+            save_name = f'noised_normal-{noise_mean:.2f}-{noise_std:.2f}_datasets_samples-{n_samples}_ids-{id_clean}-{id_backdoored}.png'
+            title = f'N(mean={noise_mean:.2f}, std={noise_std:.2f}) pixels\n' \
+                    f'clean conf dist: m={clean_mean:.2f}, s={clean_std:.2f}\n' \
+                    f'backd conf dist: m={backdoored_mean:.2f}, s={backdoored_std:.2f}\n' \
+                    f'conf dist means difference = {conf_mean_diff:.2f}'
 
             af.overlay_two_histograms(save_path=plots_dir,
                                       save_name=save_name,
@@ -132,10 +177,7 @@ def main():
                                       second_label='backdoored model',
                                       xlabel='Confusion score',
                                       title=title)
-            print()
-            if rand_type == 'uniform':
-                print('Uniform: break')
-                sys.exit(666)
+        stats.to_csv(os.path.join(plots_dir, 'stats.csv'), index=False)
 
 
 if __name__ == '__main__':
