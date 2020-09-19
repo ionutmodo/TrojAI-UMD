7879313ecd5b73ae4eaea3810ca2860a9f2e4e3a
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-09-19 10:39:00 +0300

    added root path for cuda test

--- cuda_test.py
+++ cuda_test.py
@@ -1,12 +1,14 @@
 import numpy as np
 import torch
-
+import os
+from tools.logistics import get_project_root_path
 from tools.network_architectures import load_trojai_model, load_model
 
 device = 'cuda'
 
-path_cnn = f'/mnt/storage/Cloud/MEGA/TrojAI/TrojAI-data/round1-dataset-train/models/id-00000001/model.pt'
-path_sdn = f'/mnt/storage/Cloud/MEGA/TrojAI/TrojAI-data/round1-dataset-train/models/id-00000001/'
+path_root = get_project_root_path()
+path_cnn = os.path.join(path_root, 'TrojAI-data', 'round1-dataset-train', 'models', 'id-00000001', 'model.pt')
+path_sdn = os.path.join(path_root, 'TrojAI-data', 'round1-dataset-train', 'models', 'id-00000001')
 
 sdn_model, sdn_params = load_model(path_sdn, 'ics_train100_test0_bs25', device=device, epoch=-1)
 
