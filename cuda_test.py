cfa7a5a972d91f7de69a5ecf846aed0d35bb6a06
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-09-19 12:17:17 +0300

    step by step debug

--- cuda_test.py
+++ cuda_test.py
@@ -2,17 +2,25 @@ import numpy as np
 import torch
 import os
 from tools.logistics import get_project_root_path
-from tools.network_architectures import load_trojai_model, load_model
+from tools.network_architectures import load_trojai_model, load_model, load_params
+from architectures.SDNs.MLP import LayerwiseClassifiers
 
 device = 'cuda'
 
 path_root = get_project_root_path()
-path_cnn = os.path.join(path_root, 'TrojAI-data', 'round1-dataset-train', 'models', 'id-00000001', 'model.pt')
 path_sdn = os.path.join(path_root, 'TrojAI-data', 'round1-dataset-train', 'models', 'id-00000001')
+path_load = os.path.join(path_root, 'TrojAI-data', 'round1-dataset-train', 'models', 'id-00000001', 'ics_train100_test0_bs25', 'last')
 
-data = torch.load(path_cnn, map_location=device)
-print(data)
+print('loading model params')
+model_params = load_params(path_sdn, 'ics_train100_test0_bs25', epoch=-1)
 
-# sdn_model, sdn_params = load_model(path_sdn, 'ics_train100_test0_bs25', device=device, epoch=-1)
+print('creating LayerwiseClassifiers object')
+model = LayerwiseClassifiers(model_params['output_params'], model_params['architecture_params'])
 
-# print(sdn_model)
+print('loading model state')
+model_state = torch.load(path_load, map_location=device)
+
+print('loading ')
+model.load_state_dict(model_state, strict=False)
+
+print('script ended')
