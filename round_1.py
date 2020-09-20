b7f3802f4acb6ab5e88fe2055b14a26b49328a7a
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-09-20 22:59:55 +0300

    added code for sqrt size, original trigger color for backd and black for clean

--- round_1.py
+++ round_1.py
@@ -1,5 +1,6 @@
 import ast
 import sys
+import math
 import pickle
 import shutil
 import numpy as np
@@ -41,22 +42,24 @@ def find_most_frequently_used_trigger(path_root, metadata):
 
 def main():
     # parameters
-    torch.cuda.empty_cache()
+    # torch.cuda.empty_cache()
     test_ratio = 0
-    batch_size = 128 # for confusion experiment
+    batch_size = 16 # for confusion experiment
     device = 'cpu'
     # device = af.get_pytorch_device()
     sdn_name = 'ics_train100_test0_bs25'
     cnn_name = 'model.pt'
     sdn_type = SDNConfig.DenseNet_attach_to_DenseBlocks
+    path_trigger = 'square'
+    exp_desc = 'sqrt-size_backd-original-color_clean-black-color'
 
     # begin
     np.random.seed(666)
     path_root_project = get_project_root_path()
-    path_root = os.path.join(path_root_project, 'TrojAI-data', 'round1-dataset-train')
-    # path_root = os.path.join(path_root_project, 'TrojAI-data', 'round1-holdout-dataset')
+    # path_root = os.path.join(path_root_project, 'TrojAI-data', 'round1-dataset-train')
+    path_root = os.path.join(path_root_project, 'TrojAI-data', 'round1-holdout-dataset')
     path_metadata = os.path.join(path_root, 'METADATA.csv')
-    path_report = os.path.join(path_root, f'{os.path.basename(path_root)}-report-black-square.csv')
+    path_report = os.path.join(path_root, f'{os.path.basename(path_root)}_{exp_desc}.csv')
 
     if 'train' in os.path.basename(path_root): # append 'models' for training dataset
         path_root = os.path.join(path_root, 'models')
@@ -67,30 +70,50 @@ def main():
 
     available_architectures = ['densenet121']
 
-    df_report = pd.DataFrame(columns=['model_name', 'model_label', 'clean_mean', 'clean_std', 'backd_mean', 'backd_std'])
     n_report = 0
+    df_report = pd.DataFrame(columns=[
+        'model_name',
+        'model_architecture',
+        'model_label',
+        'trigger_color',
+        'original_trigger_size',
+        'our_trigger_size',
+        'num_classes',
+        'clean_mean',
+        'clean_std',
+        'backd_mean',
+        'backd_std'
+    ])
 
-    n_models_clean = 0
-    n_models_backd = 0
+    # n_models_clean = 0
+    # n_models_backd = 0
     print('!!! Round 1: reversing trigger color')
     for _, row in metadata.iterrows():
-        model_architecture = row['model_architecture']
         model_name = row['model_name']
+        # model_name_int = int(model_name[3:])
+        model_architecture = row['model_architecture']
         num_classes = row['number_classes']
         ground_truth = row['ground_truth']
         model_label = 'backdoor' if ground_truth else 'clean'
+
         trigger_color = row['trigger_color']
+        triggered_classes = ast.literal_eval(row['triggered_classes'].replace(' ', ', '))
+        trigger_target_class = row['trigger_target_class']
+        trigger_target_class = int(trigger_target_class) if trigger_target_class != 'None' else 0  # default class for clean models
+        trigger_size = row['trigger_size']
+
+        if len(triggered_classes) == 0:
+            triggered_classes = list(range(num_classes))
+
         if trigger_color == 'None':
             trigger_color = (0, 0, 0) # default color
         else: # reversed because round 1 uses BGR
             trigger_color = tuple(reversed(ast.literal_eval(row['trigger_color'].replace(' ', ', '))))
-        triggered_classes = ast.literal_eval(row['triggered_classes'].replace(' ', ', '))
-        if len(triggered_classes) == 0:
-            triggered_classes = list(range(num_classes))
-        trigger_target_class = row['trigger_target_class']
-        trigger_target_class = int(trigger_target_class) if trigger_target_class != 'None' else 0 # default class for clean models
-        trigger_size = row['trigger_size']
-        trigger_size = int(trigger_size) if trigger_size != 'None' else 30 # default value
+
+        if trigger_size == 'None':
+            modified_trigger_size = 4 # default value for clean models
+        else: # backdoored models have predefined size
+            modified_trigger_size = int(math.sqrt(int(trigger_size)))
 
         if model_architecture in available_architectures:
             print()
@@ -98,19 +121,10 @@ def main():
             path_model = os.path.join(path_root, model_name)
 
             path_data_clean = os.path.join(path_model, 'example_data')
-            path_data_backd = os.path.join(path_model, 'example_data_backdoored')
+            path_data_backd = os.path.join(path_model, f'backdoored_data_{exp_desc}')
 
-            if os.path.isdir(path_data_backd):# or len(os.listdir(path_data_backd)) < 2:
+            if os.path.isdir(path_data_backd):
                 shutil.rmtree(path_data_backd)
-            # if ground_truth: # model is backdoored
-            #     dir_triggers = os.path.join(path_model, 'triggers')
-            #     trigger_name = os.listdir(dir_triggers)[0] # assume only one trigger (check this for round > 1)
-            #     path_trigger = os.path.join(dir_triggers, trigger_name)
-            # else: # model is clean
-            #     path_trigger = most_frequent_trigger_path
-
-            path_trigger = 'square'
-            trigger_color = (0, 0, 0)
 
             print(f'creating backdoored dataset for {model_name}...', end=''); sys.stdout.flush()
             create_backdoored_dataset(dir_clean_data=path_data_clean,
@@ -120,7 +134,7 @@ def main():
                                       triggered_classes=triggered_classes,
                                       trigger_target_class=trigger_target_class,
                                       trigger_color=trigger_color,
-                                      trigger_size=trigger_size,
+                                      trigger_size=modified_trigger_size,
                                       p_trigger=1.0,
                                       keep_original=False)
             print('done')
@@ -154,18 +168,30 @@ def main():
             backd_mean = np.mean(confusion_backd)
             backd_std = np.std(confusion_backd)
 
-            # ['model_name', 'model_label', 'clean_mean', 'clean_std', 'backd_mean', 'backd_std']
-            df_report.loc[n_report] = [model_name, model_label, clean_mean, clean_std, backd_mean, backd_std]
+            df_report.loc[n_report] = [
+                model_name,
+                model_architecture,
+                model_label,
+                trigger_color,
+                trigger_size,
+                modified_trigger_size,
+                num_classes,
+                clean_mean,
+                clean_std,
+                backd_mean,
+                backd_std]
             n_report += 1
+
             df_report.to_csv(path_report, index=False)
 
-            n_models_clean += int(ground_truth == False)
-            n_models_backd += int(ground_truth == True)
-            if n_models_clean == 5 and n_models_backd == 5:
-                break
-
+            # n_models_clean += int(ground_truth == False)
+            # n_models_backd += int(ground_truth == True)
+            # if n_models_clean == 5 and n_models_backd == 5:
+            #     break
 
 
 if __name__ == '__main__':
     main()
     print('script ended')
+
+# TODO: change trigger size to ceil(sqrt(size)) and color, step by step
