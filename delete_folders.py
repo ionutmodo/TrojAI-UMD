eb64b942d02cd20d8c8bfb071fe2d913208d0754
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-10-13 21:52:00 +0300

    mp folder deletion

--- delete_folders.py
+++ delete_folders.py
@@ -2,7 +2,15 @@ import os
 import shutil
 import pandas as pd
 from tools.logistics import *
-from architectures.SDNs.SDNConfig import SDNConfig
+import multiprocessing as mp
+
+
+def worker_delete_folder(path):
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+        print(f'deleted {path}')
+    else:
+        print(f'!exist {path}')
 
 
 def main():
@@ -19,19 +27,18 @@ def main():
     # if 'train' in os.path.basename(root_path) and 'round1' in os.path.basename(root_path): # append 'models' for training dataset
     #     root_path = os.path.join(root_path, 'models')
 
-    count = 0
+    mp_folders = []
     for index, row in metadata.iterrows():
         model_name = row['model_name']
         # model_architecture = row['model_architecture']
         # model_id_int = int(model_name[3:])
-        
         folder = os.path.join(root_path, model_name, folder_name_to_delete)
-        if os.path.isdir(folder):
-            shutil.rmtree(folder)
-            count += 1
-            print(f'deleted ({count}) {folder}')
-        else:
-            print(f'!exist {folder}')
+        mp_folders.append(folder)
+
+    cpus = mp.cpu_count() - 4
+    print(f'Creating {len(mp_folders)} datasets using {cpus} CPU cores')
+    with mp.Pool(processes=cpus) as pool:
+        pool.map(worker_delete_folder, mp_folders)
 
 
 if __name__ == '__main__':
