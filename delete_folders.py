04f2eab45309fbdd6683b499cb5196e0e49a3849
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-10-13 21:30:24 +0300

    deleting square datasets

--- delete_folders.py
+++ delete_folders.py
@@ -6,19 +6,18 @@ from architectures.SDNs.SDNConfig import SDNConfig
 
 
 def main():
-    root_path = os.path.join(get_project_root_path(), 'TrojAI-data', 'round1-dataset-train')
+    # folder_name_to_delete = 'ics_train100_test0_bs25'
+    folder_name_to_delete = 'backdoored_data_custom-square-size-20_backd-original-color_clean-black-color'
+
+    # root_path = os.path.join(get_project_root_path(), 'TrojAI-data', 'round1-dataset-train')
     # root_path = os.path.join(get_project_root_path(), 'TrojAI-data', 'round1-holdout-dataset')
-    # root_path = os.path.join(get_project_root_path(), 'TrojAI-data', 'round2-train-dataset')
+    root_path = os.path.join(get_project_root_path(), 'TrojAI-data', 'round2-train-dataset')
 
     metadata_path = os.path.join(root_path, 'METADATA.csv')
     metadata = pd.read_csv(metadata_path)
 
-    if 'train' in os.path.basename(root_path) and 'round1' in os.path.basename(root_path): # append 'models' for training dataset
-        root_path = os.path.join(root_path, 'models')
-
-    # sdn_type, architecture_to_train = SDNConfig.ResNet50, 'resnet50'
-    # sdn_type, architecture_to_train = SDNConfig.Inception3, 'inceptionv3'
-    # sdn_type, architecture_to_train = SDNConfig.DenseNet_attach_to_DenseBlocks, 'densenet121'
+    # if 'train' in os.path.basename(root_path) and 'round1' in os.path.basename(root_path): # append 'models' for training dataset
+    #     root_path = os.path.join(root_path, 'models')
 
     count = 0
     for index, row in metadata.iterrows():
@@ -26,7 +25,7 @@ def main():
         # model_architecture = row['model_architecture']
         # model_id_int = int(model_name[3:])
         
-        ics_folder = os.path.join(root_path, model_name, 'ics_train100_test0_bs25')
+        ics_folder = os.path.join(root_path, model_name, folder_name_to_delete)
         if os.path.isdir(ics_folder):
             shutil.rmtree(ics_folder)
             count += 1
