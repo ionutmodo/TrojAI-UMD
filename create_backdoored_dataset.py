e1571d3b06445535fa6c581d01a70895201ba65a
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-10-13 18:21:33 +0300

    added support for openlab30-31-32-33 for multiprocessing

--- create_backdoored_dataset.py
+++ create_backdoored_dataset.py
@@ -1,6 +1,7 @@
 import os
 import ast
 import shutil
+import socket
 import pandas as pd
 import multiprocessing as mp
 from tools.logistics import get_project_root_path
@@ -24,7 +25,8 @@ def create_dataset_multiprocessing(dict_params):
         print(f'deleted ({files_count_in_backd_dataset}) {path_data_backd}')
 
     create_backdoored_dataset(**params_method)
-    print(f'done {path_data_backd}')
+    items = path_data_backd.split(os.path.sep)
+    print(f'done {os.path.join(items[-3], items[-2], items[-1])}')
 
 
 def main():
@@ -43,76 +45,86 @@ def main():
     list_trigger_sizes = [20]
     list_filters = ['gotham', 'kelvin', 'lomo', 'nashville']
 
+    list_limits = {
+        'openlab30.umiacs.umd.edu': (0, 275),
+        'openlab31.umiacs.umd.edu': (276, 551),
+        'openlab32.umiacs.umd.edu': (552, 827),
+        'openlab33.umiacs.umd.edu': (828, 1103)
+    }
+
     for _, row in metadata.iterrows():
         model_name = row['model_name']
-        num_classes = row['number_classes']
-        number_example_images = int(row['number_example_images'])
-        trigger_color = row['trigger_color']
-        triggered_classes = row['triggered_classes']
+        model_id = int(model_name[3:])
+        left, right = list_limits[socket.gethostname()]
+        if left <= model_id <= right:
+            num_classes = row['number_classes']
+            number_example_images = int(row['number_example_images'])
+            trigger_color = row['trigger_color']
+            triggered_classes = row['triggered_classes']
 
-        if triggered_classes.lower() == 'none':
-            triggered_classes = '[]'
-        triggered_classes = ast.literal_eval(triggered_classes.replace(' ', ', '))
-        trigger_target_class = row['trigger_target_class']
+            if triggered_classes.lower() == 'none':
+                triggered_classes = '[]'
+            triggered_classes = ast.literal_eval(triggered_classes.replace(' ', ', '))
+            trigger_target_class = row['trigger_target_class']
 
-        # default class for clean models
-        # I set trigger_target_class to 0 to pass a valid parameter to method create_backdoored_dataset
-        trigger_target_class = int(trigger_target_class) if trigger_target_class.lower() != 'none' else 0
+            # default class for clean models
+            # I set trigger_target_class to 0 to pass a valid parameter to method create_backdoored_dataset
+            trigger_target_class = int(trigger_target_class) if trigger_target_class.lower() != 'none' else 0
 
-        if len(triggered_classes) == 0:
-            triggered_classes = list(range(num_classes))
+            if len(triggered_classes) == 0:
+                triggered_classes = list(range(num_classes))
 
-        if trigger_color.lower() == 'none':
-            trigger_color = (0, 0, 0) # default color
-        else:  # reversed because round 1 uses BGR
-            trigger_color = tuple(reversed(ast.literal_eval(row['trigger_color'].replace(' ', ', '))))
+            if trigger_color.lower() == 'none':
+                trigger_color = (0, 0, 0) # default color
+            else:  # reversed because round 1 uses BGR
+                trigger_color = tuple(reversed(ast.literal_eval(row['trigger_color'].replace(' ', ', '))))
 
-        ###############################################################################################################
+            ###############################################################################################################
 
-        path_model = os.path.join(path_root, model_name)
-        if os.path.isdir(path_model):
-            path_data_clean = os.path.join(path_model, 'example_data')
-            # for a path_data_clean, generate a path_data_backd with a for a specific size for square trigger
-            # generate backdoored datasets with square trigger with specific size
-            for p_trigger_size in list_trigger_sizes:
-                exp_desc = f'custom-square-size-{p_trigger_size}_backd-original-color_clean-black-color'
-                path_data_backd = os.path.join(path_model, f'backdoored_data_{exp_desc}')
+            path_model = os.path.join(path_root, model_name)
+            if os.path.isdir(path_model):
+                path_data_clean = os.path.join(path_model, 'example_data')
+                # for a path_data_clean, generate a path_data_backd with a for a specific size for square trigger
+                # generate backdoored datasets with square trigger with specific size
+                for p_trigger_size in list_trigger_sizes:
+                    exp_desc = f'custom-square-size-{p_trigger_size}_backd-original-color_clean-black-color'
+                    path_data_backd = os.path.join(path_model, f'backdoored_data_{exp_desc}')
 
-                mapping_param_dict = dict(
-                    num_classes=num_classes,
-                    images_per_class=number_example_images,
-                    params_method=dict(
-                        dir_clean_data=path_data_clean,
-                        dir_backdoored_data=path_data_backd,
-                        trigger_type='polygon',
-                        trigger_name='square',
-                        trigger_color=trigger_color,
-                        trigger_size=p_trigger_size,
-                        triggered_classes=triggered_classes,
-                        trigger_target_class=trigger_target_class)
-                )
-                mp_mapping_params.append(mapping_param_dict)
+                    mapping_param_dict = dict(
+                        num_classes=num_classes,
+                        images_per_class=number_example_images,
+                        params_method=dict(
+                            dir_clean_data=path_data_clean,
+                            dir_backdoored_data=path_data_backd,
+                            trigger_type='polygon',
+                            trigger_name='square',
+                            trigger_color=trigger_color,
+                            trigger_size=p_trigger_size,
+                            triggered_classes=triggered_classes,
+                            trigger_target_class=trigger_target_class)
+                    )
+                    mp_mapping_params.append(mapping_param_dict)
 
-            # generate backdoored datasets with specific filter
-            for p_filter_name in list_filters:
-                path_data_backd = os.path.join(path_model, f'backdoored_data_filter_{p_filter_name}')
+                # generate backdoored datasets with specific filter
+                for p_filter_name in list_filters:
+                    path_data_backd = os.path.join(path_model, f'backdoored_data_filter_{p_filter_name}')
 
-                mapping_param_dict = dict(
-                    num_classes=num_classes,
-                    images_per_class=number_example_images,
-                    params_method=dict(
-                        dir_clean_data=path_data_clean,
-                        dir_backdoored_data=path_data_backd,
-                        trigger_type='filter',
-                        trigger_name=p_filter_name,
-                        trigger_color=None,
-                        trigger_size=None,
-                        triggered_classes=triggered_classes,
-                        trigger_target_class=trigger_target_class)
-                )
-                mp_mapping_params.append(mapping_param_dict)
+                    mapping_param_dict = dict(
+                        num_classes=num_classes,
+                        images_per_class=number_example_images,
+                        params_method=dict(
+                            dir_clean_data=path_data_clean,
+                            dir_backdoored_data=path_data_backd,
+                            trigger_type='filter',
+                            trigger_name=p_filter_name,
+                            trigger_color=None,
+                            trigger_size=None,
+                            triggered_classes=triggered_classes,
+                            trigger_target_class=trigger_target_class)
+                    )
+                    mp_mapping_params.append(mapping_param_dict)
 
-    cpus = mp.cpu_count()-4
+    cpus = mp.cpu_count() - 4
     print(f'Creating {len(mp_mapping_params)} datasets using {cpus} CPU cores')
     with mp.Pool(processes=cpus) as pool:
         pool.map(create_dataset_multiprocessing, mp_mapping_params)
