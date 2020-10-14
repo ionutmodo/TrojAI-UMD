d46857aef61dffa15c4656f5710dafbb3105b169
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-10-14 13:49:15 +0300

    added toaster filter

--- test.py
+++ test.py
@@ -16,17 +16,18 @@ import skimage.io
 import trojai.datagen.instagram_xforms as instagram
 from tools.data import _get_single_image
 
-# path = r'D:\Cloud\MEGA\TrojAI\TrojAI-data\round2-train-dataset\id-00000001\example_data\class_0_example_2.png'
-path = r'/fs/sdsatumd/ionmodo/TrojAI/TrojAI-data/round2-train-dataset/id-00000001/example_data/class_0_example_2.png'
+path = r'D:\Cloud\MEGA\TrojAI\TrojAI-data\round2-train-dataset\id-00000001\example_data\class_0_example_2.png'
+# path = r'/fs/sdsatumd/ionmodo/TrojAI/TrojAI-data/round2-train-dataset/id-00000001/example_data/class_0_example_2.png'
 image = PIL.Image.open(path)
 
 filters = {
     'gotham': instagram.GothamFilterXForm(),
     'kelvin': instagram.KelvinFilterXForm(),
     'lomo': instagram.LomoFilterXForm(),
-    'nashville': instagram.NashvilleFilterXForm()
+    'nashville': instagram.NashvilleFilterXForm(),
+    'toaster': instagram.ToasterXForm()
 }
 for name, filter in filters.items():
     image_filtered = filter.filter(wand.image.Image.from_array(image))
-    # image_filtered.save(filename=rf'C:\Users\Ionut-Vlad Modoranu\Desktop\{name}.png')
-    image_filtered.save(filename=rf'/fs/sdsatumd/ionmodo/TrojAI/TrojAI-data/{name}.png')
+    image_filtered.save(filename=rf'C:\Users\Ionut-Vlad Modoranu\Desktop\{name}.png')
+    # image_filtered.save(filename=rf'/fs/sdsatumd/ionmodo/TrojAI/TrojAI-data/{name}.png')
