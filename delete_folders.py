ae8fece42025bb11e57599d2c7dbadec0f1ba428
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-10-15 23:07:11 +0300

    deleting round1-models

--- delete_folders.py
+++ delete_folders.py
@@ -17,10 +17,10 @@ def main():
     folder_names_to_delete = [
         # 'ics_train100_test0_bs25',
         # 'backdoored_data_custom-square-size-20_backd-original-color_clean-black-color',
-        'backdoored_data_filter_gotham',
-        'backdoored_data_filter_kelvin',
-        'backdoored_data_filter_lomo',
-        'backdoored_data_filter_nashville'
+        # 'backdoored_data_filter_gotham',
+        # 'backdoored_data_filter_kelvin',
+        # 'backdoored_data_filter_lomo',
+        # 'backdoored_data_filter_nashville'
     ]
 
     # root_path = os.path.join(get_project_root_path(), 'TrojAI-data', 'round1-dataset-train')
@@ -36,11 +36,14 @@ def main():
     mp_folders = []
     for index, row in metadata.iterrows():
         model_name = row['model_name']
+
+        mp_folders.append(os.path.join(root_path, model_name))
         # model_architecture = row['model_architecture']
         # model_id_int = int(model_name[3:])
-        for f_name in folder_names_to_delete:
-            folder = os.path.join(root_path, model_name, f_name)
-            mp_folders.append(folder)
+
+        # for f_name in folder_names_to_delete:
+        #     folder = os.path.join(root_path, model_name, f_name)
+        #     mp_folders.append(folder)
 
     cpus = mp.cpu_count() - 4
     print(f'Creating {len(mp_folders)} datasets using {cpus} CPU cores')
