d4d458d17ac5b87594e135d97c73f6c8ccbf61f0
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-10-27 21:26:20 +0200

    delete backdoored folders created using triggered classes

--- delete_folders.py
+++ delete_folders.py
@@ -16,16 +16,24 @@ def worker_delete_folder(path):
 def main():
     folder_names_to_delete = [
         # 'ics_train100_test0_bs25',
-        # 'backdoored_data_custom-square-size-20_backd-original-color_clean-black-color',
-        # 'backdoored_data_filter_gotham',
-        # 'backdoored_data_filter_kelvin',
-        # 'backdoored_data_filter_lomo',
-        # 'backdoored_data_filter_nashville'
+        'backdoored_data_custom-square-size-5_backd-original-color_clean-black-color',
+        'backdoored_data_custom-square-size-10_backd-original-color_clean-black-color',
+        'backdoored_data_custom-square-size-15_backd-original-color_clean-black-color',
+        'backdoored_data_custom-square-size-25_backd-original-color_clean-black-color',
+        'backdoored_data_custom-square-size-30_backd-original-color_clean-black-color',
+        'backdoored_data_custom-square-size-35_backd-original-color_clean-black-color',
+        'backdoored_data_custom-square-size-40_backd-original-color_clean-black-color',
+        'backdoored_data_custom-square-size-45_backd-original-color_clean-black-color',
+        'backdoored_data_custom-square-size-50_backd-original-color_clean-black-color',
+        'backdoored_data_filter_gotham',
+        'backdoored_data_filter_kelvin',
+        'backdoored_data_filter_lomo',
+        'backdoored_data_filter_nashville'
     ]
 
-    root_path = os.path.join(get_project_root_path(), 'TrojAI-data', 'round1-dataset-train')
+    # root_path = os.path.join(get_project_root_path(), 'TrojAI-data', 'round1-dataset-train')
     # root_path = os.path.join(get_project_root_path(), 'TrojAI-data', 'round1-holdout-dataset')
-    # root_path = os.path.join(get_project_root_path(), 'TrojAI-data', 'round2-train-dataset')
+    root_path = os.path.join(get_project_root_path(), 'TrojAI-data', 'round2-train-dataset')
 
     metadata_path = os.path.join(root_path, 'METADATA.csv')
     metadata = pd.read_csv(metadata_path)
@@ -37,13 +45,12 @@ def main():
     for index, row in metadata.iterrows():
         model_name = row['model_name']
 
-        mp_folders.append(os.path.join(root_path, model_name))
         # model_architecture = row['model_architecture']
         # model_id_int = int(model_name[3:])
 
-        # for f_name in folder_names_to_delete:
-        #     folder = os.path.join(root_path, model_name, f_name)
-        #     mp_folders.append(folder)
+        for f_name in folder_names_to_delete:
+            folder = os.path.join(root_path, model_name, f_name)
+            mp_folders.append(folder)
 
     cpus = mp.cpu_count() - 4
     print(f'Deleting {len(mp_folders)} datasets using {cpus} CPU cores')
