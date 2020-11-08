bce028ad7662a600162698c8cf55f41bfe99ab61
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-11-08 19:00:54 +0200

    fixed typo

--- round_2.py
+++ round_2.py
@@ -58,7 +58,7 @@ def main():
     else:
         lim_left, lim_right = int(sys.argv[1]), int(sys.argv[2])
 
-    print(f'{lim_left=}, {lim_right=}')
+    print(f'lim_left={lim_left}, lim_right={lim_right}')
 
     test_ratio = 0
     batch_size = 16  # for confusion experiment
