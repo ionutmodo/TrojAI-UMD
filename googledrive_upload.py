c9646712ce26ae85183de2c1ee539a4fc8c0e645
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-12-11 23:39:33 +0200

    added google drive upload file

---
+++ googledrive_upload.py
@@ -0,0 +1,34 @@
+from pydrive.drive import GoogleDrive
+from pydrive.auth import GoogleAuth
+
+# For using listdir()
+import os
+
+
+# Below code does the authentication
+# part of the code
+gauth = GoogleAuth()
+
+# Creates local webserver and auto
+# handles authentication.
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
+
+# replace the value of this variable
+# with the absolute path of the directory
+path = 'umd_pipeline.def'
+
+# iterating thought all the files/folder
+# of the desired directory
+
+f = drive.CreateFile({'title': path})
+f.SetContentFile(path)
+f.Upload()
+
+# Due to a known bug in pydrive if we
+# don't empty the variable used to
+# upload the files to Google Drive the
+# file stays open in memory and causes a
+# memory leak, therefore preventing its
+# deletion
+f = None
