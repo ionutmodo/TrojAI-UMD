6e0e843bac08f8d5e2fd071b070171a40a61a735
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2020-12-12 01:14:37 +0200

    updated title

--- googledrive_upload.py
+++ googledrive_upload.py
@@ -9,6 +9,6 @@ drive = GoogleDrive(gauth)
 
 file_name = sys.argv[1]
 
-file = drive.CreateFile({'title': f'test/{os.path.basename(file_name)}'})
+file = drive.CreateFile({'title': os.path.basename(file_name)})
 file.SetContentFile(file_name)
 file.Upload()
