c1526625c09c195b0a5c47bca4cc6c1e70827e33
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2021-01-30 22:28:18 +0200

    moved while True in if

--- generate_conf_dist_wrt_T.py
+++ generate_conf_dist_wrt_T.py
@@ -155,20 +155,20 @@ def main():
         model_name = row['model_name']
         model_id = int(model_name[3:])
 
-        # wait for the round_features_sdn process to create the npz files for each model
-        lookup_folder = os.path.join(synthetic_stats_root, model_name)
-        while True:
-            if not os.path.isdir(lookup_folder):
-                Logger.log(f'Waiting for folder synthetic_stats/{model_name} to be created...')
-                time.sleep(60)
-                continue
-            files_count = len(os.listdir(lookup_folder))
-            if files_count == 7:
-                break
-            Logger.log(f'Waiting for folder synthetic_stats/{model_name} to have 7 files...')
-            time.sleep(60)
-
         if lim_left <= model_id <= lim_right and ((last_model_name_in_report_conf_dist is None) or (last_model_name_in_report_conf_dist is not None and model_name > last_model_name_in_report_conf_dist)):
+            # wait for the round_features_sdn process to create the npz files for each model
+            lookup_folder = os.path.join(synthetic_stats_root, model_name)
+            while True:
+                if not os.path.isdir(lookup_folder):
+                    Logger.log(f'Waiting for folder synthetic_stats/{model_name} to be created...')
+                    time.sleep(60)
+                    continue
+                files_count = len(os.listdir(lookup_folder))
+                if files_count == 7:
+                    break
+                Logger.log(f'Waiting for folder synthetic_stats/{model_name} to have 7 files...')
+                time.sleep(60)
+
             backd_str, backd_0_code, backd_1_code = get_trigger_type_aux_value(row['triggers_0_type'], row['triggers_0_instagram_filter_type'], row['triggers_1_type'], row['triggers_1_instagram_filter_type'])
             model_label = 'backdoor' if row['poisoned'] else 'clean'
             model_architecture = row['model_architecture']
